99f007b9fcaa3ef59d4afde96a34757804c386f2
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2022-05-08 16:44:56 -0300

    Começando a trabalhar com responsividade

--- index.php
+++ index.php
@@ -7,60 +7,32 @@
         <meta name="robots" content="noindex">
         <meta name="description" content="Sistema para jogos internos">    
         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/viewport.css">
         <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     </head>
     <body>
-        <div class="conteudo"> 
-            <div class="base-central">
-                <div class="base-topo">
-                    <a href="index.php" class="titulo-home">SISTEMA DE JOGOS INTERNOS</a>
+       <header>
+            <div class="header-content">
+                <div class="header-titulo">
+                    </h1 class="header-titulo">Indoor-games</h1>
                 </div>
-                <nav class="menu">
-                    <ul>
-                        <li><a href="index.php">Home</a>|</li>
-                        <li><a href="index.php">Cadastrar novo</a>|</li>
-                        <li><a href="index.php">Lista de Jogadores</a></li>
-                    </ul>
-                </nav>
-                <div class="base-home">
-                    <h1 class="titulo"><span>Seja </span>bem vindo</h1>
-                    <div class="base-colunas">
-                        <div>
-                            <a href="#" class="col">
-                                <i class="icone ico1">
-                                    <span><b>Novo </b> Cadastro</span>
-                                </i>
-                            </a>
-                            <a href="#" class="col">
-                                <i class="icone ico2">
-                                    <span>
-                                        <b>Lista</b> de Jogadores
-                                    </span>
-                                </i>
-                            </a>
-                            <a href="#" class="col">
-                                <i class="icone ico2">
-                                    <span>
-                                        <b>Lista</b> de Jogadores
-                                    </span>
-                                </i>
-                            </a>
-                            <a href="#" class="col">
-                                <i class="icone ico2">
-                                    <span>
-                                        <b>Lista</b> de Jogadores
-                                    </span>
-                                </i>
-                            </a>
-                        </div>
-                    </div>
+                <div class="header-menu">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home</a> <span> | </span></li>
+                            <li><a href="#">times</a><span> | </span></li>
+                            <li><a href="#">jogadores</a></li>
+                        </ul>
+                    </nav> 
                 </div>
-
-                <div class="rodape">
-                    <p>Direitos reservado @gustavoandradedeveloper.com </p>
-                </div>
-
             </div>
-        </div>
+       </header>
+       <main>
+
+       </main>
+       <footer>
+
+       </footer>
     </body>
 </html>
